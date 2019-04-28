bf2e0f4c1659366b1d53dfd0295966644d8a5c3e
airts <jlrogee@gmail.com>
2019-04-28 16:36:11 +0300

    init

---
+++ src/Controllers/LinksController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace src.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinksController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new[] {"link"};
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(string id)
+        {
+            return "link";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
